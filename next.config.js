56259c20f77d66bc846b68f62d589558984ea9e7
gaurav jadhav <gauravjadhav7512@gmail.com>
2022-04-20 20:23:30 +0530

    vercel

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@ module.exports = {
   images: {
     domains: ["res.cloudinary.com","imagesvc.meredithcorp.io","www.allrecipes.com"],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://api.example.com/:path*',
-      },
-    ]
-  },
+ 
+
 
 };
